9f30d5c8c7380abbae2d229a7c1bc137a97686d9
rugerclaus <rogerrock12games@gmail.com>
2026-08-27 04:54:33 -0600

    updated build system

--- core/engine/telemetry.py
+++
@@ -1,9 +0,0 @@
-system_monitor ={
-    "Distant Realms Version": None,
-    "network": None,
-    "ClientConnected": None,
-    "clientAppPassword": None,
-    "OS": None,
-    "Client Version" : None,
-    "Audio": None
-}
